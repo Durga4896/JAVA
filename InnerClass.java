9b37d90cfc35bd16062fefad9f8d453b7af7346f
DURGA SAI PRASAD YALAMKAYALA <durgasaiprasadyalamkayala@DURGAs-MacBook-Air.local>
2026-03-29 09:31:27 +0530

    InnerClass Part1 Done

---
+++ InnerClass.java
@@ -0,0 +1,20 @@
+public class InnerClass {
+    public static void main(String[] dspy){
+        System.out.println("Class!");
+    }
+}
+class Inner1{
+    public static void main(String[] dspy){
+        System.out.println("Inner1!");
+    }
+}
+class Inner2{
+    public static void main(String[] dspy){
+        System.out.println("Inner2!");
+    }
+}
+class Inner3{
+    public static void main(String[] dspy){
+        System.out.println("Inner3!");
+    }
+}
